9eb53e619faae0ca26e4b3ca64061c19a22fa9cd
metacortex687 <metacortex687@gmail.com>
2026-01-10 19:03:31 +0700

    переименован эндпоинт для получения списка транспортных средств во множественное число

--- taxi_manager/api_v1/urls.py
+++ taxi_manager/api_v1/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import VehicleListAPIView
 
 urlpatterns = [
+    path('vehicles/', VehicleListAPIView.as_view()),
     path('vehicles/<int:pk>/', VehicleDetailAPIView.as_view()),
     path('models/<int:pk>/', ModelDetailAPIView.as_view()),
 ]
