92f2e018a47e112b5878414946ae1a6e76829c14
metacortex687 <metacortex687@gmail.com>
2026-01-10 17:42:34 +0700

    модель представлена полем model_id

--- taxi_manager/api_v1/serializers.py
+++ taxi_manager/api_v1/serializers.py
@@ -3,17 +3,17 @@ from ..vehicle.models import Vehicle, Model
 
 
 class VehicleSerializer(serializers.ModelSerializer):
-    # model_id = serializers.SerializerMethodField()
+    model_id = serializers.SerializerMethodField()
     # model_name = serializers.SerializerMethodField()
     # model = ModelSerializer()
 
-    # def get_model_id(self,obj):
-    #     return obj.model.id
+    def get_model_id(self,obj):
+        return obj.model.id
 
     # def get_model_name(self,obj):
     #     return obj.model.name
     
     class Meta:
-        depth = 1
+        #depth = 1
         model = Vehicle
-        fields = ("id", "number", "vin", "year_of_manufacture", "mileage", "price", "created_at", "updated_at", "model" )
+        fields = ("id", "model_id", "number", "vin", "year_of_manufacture", "mileage", "price", "created_at", "updated_at",  )
