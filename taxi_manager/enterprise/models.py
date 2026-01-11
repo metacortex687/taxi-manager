a41ece2aa01b0e5cea81cd6571e1df6c49aca636
metacortex687 <metacortex687@gmail.com>
2026-01-12 00:07:09 +0700

    модель водителя

--- taxi_manager/enterprise/models.py
+++ taxi_manager/enterprise/models.py
@@ -4,3 +4,11 @@ class Enterprise(models.Model):
     name = models.CharField(max_length=25, verbose_name="Наименование", unique=True)
     city = models.CharField(max_length=25, verbose_name="Город")
 
+
+class Driver(models.Model):
+    first_name = models.CharField(max_length=35, verbose_name="Имя")
+    last_name = models.CharField(max_length=35, verbose_name="Фвмилия")
+    TIN = models.CharField(max_length=12, verbose_name="ИНН", unique=True) 
+
+    enterprise = models.ForeignKey(Enterprise, on_delete=models.RESTRICT)
+
