5e9e70ad0dc631be2a3279a6ffd6b7e7a19c8bb3
metacortex687 <metacortex687@gmail.com>
2026-01-12 00:25:58 +0700

    отношение многие ко многим для автомобилей и водителей

--- taxi_manager/enterprise/models.py
+++ taxi_manager/enterprise/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ..vehicle.models import Vehicle
 
 class Enterprise(models.Model):
     name = models.CharField(max_length=25, verbose_name="Наименование", unique=True)
@@ -11,4 +12,13 @@ class Driver(models.Model):
     TIN = models.CharField(max_length=12, verbose_name="ИНН", unique=True) 
 
     enterprise = models.ForeignKey(Enterprise, on_delete=models.RESTRICT)
+
+    vehicles = models.ManyToManyField(Vehicle, through="VehicleDriver", through_fields=("driver", "vehicle"), related_name='drivers')
+
+
+class VehicleDriver(models.Model):
+    driver  = models.ForeignKey(Driver, on_delete=models.CASCADE) 
+    vehicle = models.ForeignKey(Driver, on_delete=models.CASCADE)    
+
+
 
