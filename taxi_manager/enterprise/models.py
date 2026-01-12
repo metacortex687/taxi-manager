763e39f334b7fadb7f9422afce69ea8c5bd1b001
metacortex687 <metacortex687@gmail.com>
2026-01-12 21:56:31 +0700

    строковое представление организации

--- taxi_manager/enterprise/models.py
+++ taxi_manager/enterprise/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 class Enterprise(models.Model):
-    name = models.CharField(max_length=25, verbose_name="Наименование", unique=True)
+    name = models.CharField(max_length=35, verbose_name="Наименование", unique=True)
     city = models.CharField(max_length=25, verbose_name="Город")
+
+
+    def __str__(self):
+        return f"{self.name} ({self.city})"
