d755878a0147cae562045fe5dcff502a820a16b8
metacortex687 <metacortex687@gmail.com>
2026-01-12 11:36:03 +0700

    отображение id предприятия для автомобиля

--- taxi_manager/api_v1/serializers.py
+++ taxi_manager/api_v1/serializers.py
@@ -4,19 +4,22 @@ from ..vehicle.models import Vehicle, Model
 
 class VehicleSerializer(serializers.ModelSerializer):
     model_id = serializers.SerializerMethodField()
+    enterprise_id = serializers.SerializerMethodField()
     # model_name = serializers.SerializerMethodField()
     # model = ModelSerializer()
 
     def get_model_id(self,obj):
         return obj.model.id
 
-    # def get_model_name(self,obj):
-    #     return obj.model.name
+    def get_enterprise_id(self,obj):
+        if not obj.enterprise:
+            return None
+        return obj.enterprise.id
     
     class Meta:
         #depth = 1
         model = Vehicle
-        fields = ("id", "model_id", "number", "vin", "year_of_manufacture", "mileage", "price", "created_at", "updated_at",  )
+        fields = ("id", "model_id", "number", "vin", "year_of_manufacture", "mileage", "price", "enterprise_id", "created_at", "updated_at",  )
 
 
 class ModelSerializer(serializers.ModelSerializer):
