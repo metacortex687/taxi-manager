283be7dff4b7cac781ebf6ebfe7c13e7da118f95
metacortex687 <metacortex687@gmail.com>
2026-01-13 16:18:19 +0700

    убрал пустую строку автоформатирвоанием

--- taxi_manager/enterprise/models.py
+++ taxi_manager/enterprise/models.py
@@ -5,6 +5,5 @@ class Enterprise(models.Model):
     name = models.CharField(max_length=35, verbose_name="Наименование", unique=True)
     city = models.CharField(max_length=25, verbose_name="Город")
 
-
     def __str__(self):
         return f"{self.name} ({self.city})"
