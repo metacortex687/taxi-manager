38c6f0f97a8dc049b073ca75e32075312cb299aa
metacortex687 <metacortex687@gmail.com>
2026-01-14 11:31:55 +0700

    настройка списка в админке для таблицы Предприятия

--- taxi_manager/enterprise/admin.py
+++ taxi_manager/enterprise/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Enterprise
 # Register your models here.
-admin.site.register(Enterprise)
+
+class EnterpriseAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "city",
+    )
+
+admin.site.register(Enterprise, EnterpriseAdmin)
