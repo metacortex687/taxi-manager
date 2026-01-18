99e13561a6447b30f0c0c79e5d103231339edf3d
metacortex687 <metacortex687@gmail.com>
2026-01-18 20:40:51 +0700

    создана модель менеджеры

--- taxi_manager/enterprise/models.py
+++ taxi_manager/enterprise/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class Enterprise(models.Model):
     name = models.CharField(max_length=35, verbose_name="Наименование", unique=True)
     city = models.CharField(max_length=25, verbose_name="Город")
+    manager_users = models.ManyToManyField(
+        User,
+        through="Manager",
+        through_fields=("enterprise", "user"),
+        related_name="managed_enterprises",
+    )
 
     def __str__(self):
         return f"{self.name} ({self.city})"
@@ -11,3 +19,17 @@ class Enterprise(models.Model):
     class Meta:
         verbose_name = "Предприятие"
         verbose_name_plural = "Предприятия"
+
+
+class Manager(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.RESTRICT, verbose_name="Пользователь"
+    )
+    enterprise = models.ForeignKey(
+        Enterprise, on_delete=models.RESTRICT, verbose_name="Предприятие"
+    )
+
+    class Meta:
+        verbose_name = "Менеджер"
+        verbose_name_plural = "Менеджеры"
+        unique_together = ("user", "enterprise")
