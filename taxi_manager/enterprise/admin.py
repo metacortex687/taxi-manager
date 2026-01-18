5c9f33d067dbd30594c40ff3cba0a38a0c897d52
metacortex687 <metacortex687@gmail.com>
2026-01-18 20:41:13 +0700

    отображение модели менеджеров в админке

--- taxi_manager/enterprise/admin.py
+++ taxi_manager/enterprise/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Enterprise
+from .models import Enterprise, Manager
 # Register your models here.
 
 class EnterpriseAdmin(admin.ModelAdmin):
@@ -9,4 +9,12 @@ class EnterpriseAdmin(admin.ModelAdmin):
         "city",
     )
 
+class ManagerAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "enterprise",
+        "user",
+    )
+
 admin.site.register(Enterprise, EnterpriseAdmin)
+admin.site.register(Manager, ManagerAdmin)
