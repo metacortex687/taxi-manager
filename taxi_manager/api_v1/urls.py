0532f86ae27da1372d593c8f27551867ff8dcc25
metacortex687 <metacortex687@gmail.com>
2026-01-20 13:40:24 +0700

    автоформатирование

--- taxi_manager/api_v1/urls.py
+++ taxi_manager/api_v1/urls.py
@@ -1,6 +1,27 @@
 from django.urls import path, include
-from .views import VehicleListAPIView, ModelListAPIView, VehicleDetailAPIView, ModelDetailAPIView, DriverListAPIView, DriverDetailAPIView, EnterpriseDetailAPIView, EnterpriseListAPIView
+from .views import (
+    VehicleListAPIView,
+    ModelListAPIView,
+    VehicleDetailAPIView,
+    ModelDetailAPIView,
+    DriverListAPIView,
+    DriverDetailAPIView,
+    EnterpriseDetailAPIView,
+    EnterpriseListAPIView,
+    SessionLogoutView
+)
+
 
 urlpatterns = [
+    path("vehicles/", VehicleListAPIView.as_view()),
+    path("models/", ModelListAPIView.as_view()),
+    path("vehicles/<int:pk>/", VehicleDetailAPIView.as_view()),
+    path("models/<int:pk>/", ModelDetailAPIView.as_view()),
+    path("drivers/", DriverListAPIView.as_view()),
+    path("enterprises/", EnterpriseListAPIView.as_view()),
+    path("drivers/<int:driver_id>/vehicles/", VehicleListAPIView.as_view()),
+    path("drivers/<int:pk>/", DriverDetailAPIView.as_view()),
+    path("enterprises/<int:pk>/", EnterpriseDetailAPIView.as_view()),
     path("logout/", SessionLogoutView.as_view()),
+    path("", include("rest_framework.urls", namespace="rest_framework")),
 ]
