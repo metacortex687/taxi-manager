547c61ae09bf90f5df3531afc73424eeeba15dca
metacortex687 <metacortex687@gmail.com>
2026-01-29 22:19:29 +0700

    fix: При указаниии неверного логина и пароля при получении токена, код возврата должен быть 401 вместо 400.

--- taxi_manager/api_v1/utils.py
+++ taxi_manager/api_v1/utils.py
@@ -3,8 +3,26 @@ from django import VERSION as DJANGO_VERSION
 from rest_framework import exceptions
 
 
+def is_invalid_credentials(detail):
+    non_field_errors = detail.get("non_field_errors")
+
+    if not non_field_errors:
+        return False
+
+    for error in non_field_errors:
+        if error.code == "invalid_credentials":
+            return True
+
+    return False
+
+
 def custom_exception_handler(exc, context):
     if isinstance(exc, exceptions.NotAuthenticated): #Исправление проблемы то что исключение NotAuthenticated возвращало 403, а не 401.
         exc = exceptions.NotAuthenticated(*(exc.args))
+
+    if isinstance(exc, exceptions.ValidationError) and is_invalid_credentials(
+        exc.detail
+    ): # Код возврата когда ошибки учетных данных при получении токена, должен быть 401 а не 403
+        exc = exceptions.AuthenticationFailed(*(exc.args))
 
     return exception_handler(exc, context)
