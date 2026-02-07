7c577fde9df0f4ca6cf808303d846f4074f75d32
metacortex687 <metacortex687@gmail.com>
2026-02-07 16:20:49 +0700

    Команда generate_data для manage.py создания предприятий в базе

---
+++ taxi_manager/enterprise/management/commands/generate_data.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from ...models import Enterprise
+
+class Command(BaseCommand):
+    help = "Добавление в базу сгенерированных данных"
+
+    def add_arguments(self, parser):
+        parser.add_argument("-enterprise", nargs="+", type=str, help="Перечислите имена создаваемых предприятий")
+
+
+    def handle(self, *args, **options):        
+
+        if type(options["enterprise"]) is str:
+            enterprise_name_list = [options["enterprise"]]        
+
+        if type(options["enterprise"]) is list:
+            enterprise_name_list = options["enterprise"]
+
+        for name in enterprise_name_list:
+            Enterprise.objects.create(name = name, city = "City")
+
--- taxi_manager/enterprise/tests.py
+++ taxi_manager/enterprise/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.core.management import call_command
+from .models import Enterprise
 
-# Create your tests here.
+class CommandGenerateDataTest(TestCase):
+    def test_generate_one_enterprise(self):
+        name_enterprise = "test_name"
+
+        self.assertFalse(Enterprise.objects.filter(name=name_enterprise).exists())
+
+        call_command("generate_data", enterprise = name_enterprise)
+
+        self.assertTrue(Enterprise.objects.filter(name=name_enterprise).exists())
+
+
+    def test_generate_enterprises(self):
+        name_enterprise = ["test_name1", "test_name2"]
+
+        self.assertFalse(Enterprise.objects.filter(name=name_enterprise).exists())
+
+        call_command("generate_data", enterprise = name_enterprise)
+
+        self.assertTrue(Enterprise.objects.filter(name=name_enterprise[0]).exists())
+        self.assertTrue(Enterprise.objects.filter(name=name_enterprise[1]).exists())
