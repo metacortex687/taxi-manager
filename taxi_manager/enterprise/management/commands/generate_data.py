bafc7cc25f74231a616bf60b011c5f021e4bae44
metacortex687 <metacortex687@gmail.com>
2026-02-08 13:17:35 +0700

    автоформатирование

--- taxi_manager/enterprise/management/commands/generate_data.py
+++ taxi_manager/enterprise/management/commands/generate_data.py
@@ -10,35 +10,59 @@ class Command(BaseCommand):
     help = "Добавление в базу сгенерированных данных"
 
     def add_arguments(self, parser):
-        parser.add_argument("-e", "--enterprise", nargs="+", type=str, help="Перечислите имена создаваемых предприятий")
-        parser.add_argument("-d", "--driver",  type=int, help="Колличество водителей в создаваемых предприятиях")
-        parser.add_argument("-vh", "--vehicle",  type=int, help="Колличество автомобилей в создаваемых предприятиях")
-        parser.add_argument("--seed",  type=int, help="Параметр инициализации генератора псевдослучайных чисел")
+        parser.add_argument(
+            "-e",
+            "--enterprise",
+            nargs="+",
+            type=str,
+            help="Перечислите имена создаваемых предприятий",
+        )
+        parser.add_argument(
+            "-d",
+            "--driver",
+            type=int,
+            help="Колличество водителей в создаваемых предприятиях",
+        )
+        parser.add_argument(
+            "-vh",
+            "--vehicle",
+            type=int,
+            help="Колличество автомобилей в создаваемых предприятиях",
+        )
+        parser.add_argument(
+            "--seed",
+            type=int,
+            help="Параметр инициализации генератора псевдослучайных чисел",
+        )
 
-
-    def handle(self, *args, **options):   
-
+    def handle(self, *args, **options):
         if not options["enterprise"]:
-            raise CommandError("Параметр -e или --enterprise со списком создаваемых организаций обязателен.")     
+            raise CommandError(
+                "Параметр -e или --enterprise со списком создаваемых организаций обязателен."
+            )
 
         if not options["driver"]:
-            raise CommandError("Параметр -d или --driver с количеством создаваемых водителей обязателен.")     
-        
+            raise CommandError(
+                "Параметр -d или --driver с количеством создаваемых водителей обязателен."
+            )
+
         if not options["vehicle"]:
-            raise CommandError("Параметр -vh или --vehicle с количеством создаваемых автомобилей обязателен.")     
+            raise CommandError(
+                "Параметр -vh или --vehicle с количеством создаваемых автомобилей обязателен."
+            )
 
         if type(options["enterprise"]) is str:
-            enterprise_name_list = [options["enterprise"]]        
+            enterprise_name_list = [options["enterprise"]]
 
         if type(options["enterprise"]) is list:
             enterprise_name_list = options["enterprise"]
 
         enterprises = []
         for name in enterprise_name_list:
-            enterprise = Enterprise.objects.create(name = name, city = "City")
+            enterprise = Enterprise.objects.create(name=name, city="City")
             enterprises.append(enterprise)
 
-        fake = Faker(["en_US","ru_RU"])
+        fake = Faker(["en_US", "ru_RU"])
         Faker.seed(0)
         random.seed(0)
         if options["seed"]:
@@ -47,26 +71,74 @@ class Command(BaseCommand):
 
         drivers = []
         for i in range(options["driver"]):
-            name = fake.unique.name().split(' ')
-            driver = Driver.objects.create(first_name = name[0] , last_name = name[1], enterprise = enterprises[i % len(enterprises)], TIN = fake.bothify(text="############"))
+            name = fake.unique.name().split(" ")
+            driver = Driver.objects.create(
+                first_name=name[0],
+                last_name=name[1],
+                enterprise=enterprises[i % len(enterprises)],
+                TIN=fake.bothify(text="############"),
+            )
             drivers.append(driver)
-        
 
         models = []
-        models.append(Model.objects.create(name = "Нива внедорожник", type = "PCR", number_of_seats = 5, tank_capacity_l = 42, load_capacity_kg = 400))
-        models.append(Model.objects.create(name = "Камаз", type = "LRR", number_of_seats = 2, tank_capacity_l = 500, load_capacity_kg = 5700))
-        models.append(Model.objects.create(name = "Икарус", type = "BUS", number_of_seats = 34, tank_capacity_l = 250, load_capacity_kg = 5000))
-        models.append(Model.objects.create(name = "BMW", type = "PCR", number_of_seats = 5, tank_capacity_l = 40, load_capacity_kg = 420))
-        models.append(Model.objects.create(name = "Ford", type = "PCR", number_of_seats = 5, tank_capacity_l = 45, load_capacity_kg = 400))
-
+        models.append(
+            Model.objects.create(
+                name="Нива внедорожник",
+                type="PCR",
+                number_of_seats=5,
+                tank_capacity_l=42,
+                load_capacity_kg=400,
+            )
+        )
+        models.append(
+            Model.objects.create(
+                name="Камаз",
+                type="LRR",
+                number_of_seats=2,
+                tank_capacity_l=500,
+                load_capacity_kg=5700,
+            )
+        )
+        models.append(
+            Model.objects.create(
+                name="Икарус",
+                type="BUS",
+                number_of_seats=34,
+                tank_capacity_l=250,
+                load_capacity_kg=5000,
+            )
+        )
+        models.append(
+            Model.objects.create(
+                name="BMW",
+                type="PCR",
+                number_of_seats=5,
+                tank_capacity_l=40,
+                load_capacity_kg=420,
+            )
+        )
+        models.append(
+            Model.objects.create(
+                name="Ford",
+                type="PCR",
+                number_of_seats=5,
+                tank_capacity_l=45,
+                load_capacity_kg=400,
+            )
+        )
 
         vehicles = []
         for i in range(options["vehicle"]):
-            vehicle = Vehicle.objects.create(price = random.randint(500,6000000), year_of_manufacture = random.randint(1980,2026), 
-                               mileage = random.randint(1,300000), number = fake.bothify(text="?###??"),
-                               vin = fake.vin(), model = models[0], enterprise = enterprises[0])
+            vehicle = Vehicle.objects.create(
+                price=random.randint(500, 6000000),
+                year_of_manufacture=random.randint(1980, 2026),
+                mileage=random.randint(1, 300000),
+                number=fake.bothify(text="?###??"),
+                vin=fake.vin(),
+                model=models[0],
+                enterprise=enterprises[0],
+            )
             vehicles.append(vehicle)
-
 
         random.shuffle(vehicles)
         random.shuffle(drivers)
@@ -78,11 +150,12 @@ class Command(BaseCommand):
                 grouped_by_enterprise_data[vehicle.enterprise] = [], []
             _vehicles, _drivers = grouped_by_enterprise_data[vehicle.enterprise]
 
-            if random.randint(0,3) != 0: #Каждое четвертое транспортное средство будет без прикрепленных водителей
+            if (
+                random.randint(0, 3) != 0
+            ):  # Каждое четвертое транспортное средство будет без прикрепленных водителей
                 _vehicles.append(vehicle)
 
-            grouped_by_enterprise_data[vehicle.enterprise] = _vehicles, _drivers 
-
+            grouped_by_enterprise_data[vehicle.enterprise] = _vehicles, _drivers
 
         for driver in drivers:
             if driver.enterprise not in grouped_by_enterprise_data:
@@ -90,10 +163,10 @@ class Command(BaseCommand):
 
             _vehicles, _drivers = grouped_by_enterprise_data[vehicle.enterprise]
 
-            if random.randint(0,3) != 0: #Каждый четвертый водитель без автомобилей
+            if random.randint(0, 3) != 0:  # Каждый четвертый водитель без автомобилей
                 _drivers.append(driver)
 
-            grouped_by_enterprise_data[vehicle.enterprise] = _vehicles, _drivers 
+            grouped_by_enterprise_data[vehicle.enterprise] = _vehicles, _drivers
 
 
 
