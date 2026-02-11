105758f61269088c60a6d991a3777721a366e34d
metacortex687 <metacortex687@gmail.com>
2026-02-11 17:30:12 +0700

    feat/refact: Размер странинцы пагинации по умолчанию изменил с 10 на 100. (feat/refact: обозначил изменение  поведения)

--- taxi_manager/settings.py
+++ taxi_manager/settings.py
@@ -157,5 +157,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 100
 }
