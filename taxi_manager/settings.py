a600fe7ce093633022e7e8382f4d59d7d7c3b7e7
metacortex687 <metacortex687@gmail.com>
2026-02-12 18:22:32 +0700

    feat: REST выдачу можно отфильтровать указав параметр в запросе

--- taxi_manager/settings.py
+++ taxi_manager/settings.py
@@ -157,5 +157,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_FILTER_BACKENDS': ['rest_framework.filters.OrderingFilter'],
 }
