65c16bf883b988be7887615f223af8b3306960a6
metacortex687 <metacortex687@gmail.com>
2026-02-12 18:24:58 +0700

    fix: Поправил поле значение цвета по умолчанию в модели..

--- taxi_manager/vehicle/models.py
+++ taxi_manager/vehicle/models.py
@@ -98,7 +98,7 @@ class Model(models.Model):
         verbose_name="Грузоподъемность", help_text="Грузоподъемность (кг)"
     )
 
-    color = models.CharField(max_length = 30, default=True, blank=True)
+    color = models.CharField(max_length = 30, default="", blank=True)
 
     def __str__(self):
         return self.name
