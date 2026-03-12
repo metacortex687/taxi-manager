9460cc560be58836d42aa83313d77ddbd9442af9
metacortex687 <metacortex687@gmail.com>
2026-03-12 22:16:52 +0700

    Поле временная зона добавлено в предприятие

--- taxi_manager/enterprise/models.py
+++ taxi_manager/enterprise/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from taxi_manager.time_zones.models import TimeZone
 
 User = get_user_model()
 
@@ -7,6 +8,7 @@ User = get_user_model()
 class Enterprise(models.Model):
     name = models.CharField(max_length=35, verbose_name="Наименование", unique=True)
     city = models.CharField(max_length=25, verbose_name="Город")
+    time_zone = models.ForeignKey(TimeZone, default=TimeZone.UTC, on_delete=models.RESTRICT)
 
     manager_users = models.ManyToManyField(
         User,
@@ -30,6 +32,7 @@ class Manager(models.Model):
     enterprise = models.ForeignKey(
         Enterprise, on_delete=models.RESTRICT, verbose_name="Предприятие"
     )
+    
 
     class Meta:
         verbose_name = "Менеджер"
