f0f1832d3de90619560c1acd32ddbb8330b553e6
metacortex687 <metacortex687@gmail.com>
2026-03-12 22:12:40 +0700

    Добавлена таблица временных зон

--- taxi_manager/time_zones/admin.py
+++ taxi_manager/time_zones/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import TimeZone
 # Register your models here.
+
+class TimeZoneAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "code",
+        "utc_offset",
+        "display_name",
+    )
+
+admin.site.register(TimeZone, TimeZoneAdmin)
+
--- taxi_manager/time_zones/models.py
+++ taxi_manager/time_zones/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class TimeZone(models.Model):
+    code = models.CharField(max_length=64)
+    utc_offset = models.IntegerField()
+
+    @property
+    def display_name(self):
+        sign = "+" if self.utc_offset>=0 else "-"
+        str_offset = str(self.utc_offset).zfill(2)
+        return f"(UTC{sign}{str_offset}:00) {self.code}"
+    
+    def UTC():
+        return 1 #первая запись должна быть UTC
+
+
