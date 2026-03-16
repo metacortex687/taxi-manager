f6c4e0e1359bb03354966774bb54027633230c54
metacortex687 <metacortex687@gmail.com>
2026-03-16 18:32:28 +0700

    Это географическая точка, а не геометрическая.

--- taxi_manager/geo_tracking/models.py
+++ taxi_manager/geo_tracking/models.py
@@ -3,7 +3,7 @@ from taxi_manager.vehicle.models import Vehicle
 
 class VehicleLocation(models.Model):
     vehicle = models.ForeignKey(Vehicle,on_delete=models.RESTRICT, related_name="locations", verbose_name="Автомобиль")
-    location = models.PointField(srid=4326) #4326 идентификатор системы координат для GPS
+    location = models.PointField(srid=4326, geography=True) #4326 идентификатор системы координат для GPS
     tracked_at = models.DateTimeField()
 
 
