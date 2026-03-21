c72fbdad7880ab3446625c65e5e84754e16639a1
metacortex687 <metacortex687@gmail.com>
2026-03-21 08:40:36 +0700

    автоформатирование

--- taxi_manager/geo_tracking/models.py
+++ taxi_manager/geo_tracking/models.py
@@ -1,16 +1,26 @@
 from django.contrib.gis.db import models
 from taxi_manager.vehicle.models import Vehicle
 
+
 class VehicleLocation(models.Model):
-    vehicle = models.ForeignKey(Vehicle,on_delete=models.RESTRICT, related_name="locations", verbose_name="Автомобиль")
-    location = models.PointField(srid=4326, geography=True) #4326 идентификатор системы координат для GPS
+    vehicle = models.ForeignKey(
+        Vehicle,
+        on_delete=models.RESTRICT,
+        related_name="locations",
+        verbose_name="Автомобиль",
+    )
+    location = models.PointField(
+        srid=4326, geography=True
+    )  # 4326 идентификатор системы координат для GPS
     tracked_at = models.DateTimeField()
 
+
 class Trip(models.Model):
-    vehicle = models.ForeignKey(Vehicle,on_delete=models.RESTRICT, related_name="trips", verbose_name="Автомобиль")
+    vehicle = models.ForeignKey(
+        Vehicle,
+        on_delete=models.RESTRICT,
+        related_name="trips",
+        verbose_name="Автомобиль",
+    )
     started_at = models.DateTimeField()
-    ended_at = models.DateTimeField() 
-
-
-
-
+    ended_at = models.DateTimeField()
