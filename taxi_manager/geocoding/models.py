54e419d24c8ea514d7079a53157006c399db15fa
metacortex687 <metacortex687@gmail.com>
2026-03-25 10:09:00 +0700

    Добавлена модель для хранения адресов с географической привязкой

--- taxi_manager/geocoding/models.py
+++ taxi_manager/geocoding/models.py
@@ -1,3 +1,5 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class GeoAddress(models.Model):
+    display_name = models.CharField(max_length=500)
+    area = models.PolygonField(srid=4326, geography=True)
