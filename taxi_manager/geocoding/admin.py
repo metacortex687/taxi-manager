b13d4febf496a1e710264e92e1bddb93be7e5fb2
metacortex687 <metacortex687@gmail.com>
2026-03-26 10:59:40 +0700

    Отображение  адресов в админке.

--- taxi_manager/geocoding/admin.py
+++ taxi_manager/geocoding/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 
+from .models import GeoAddress
+
+class GeoAddressAdmin(admin.GISModelAdmin):
+    list_display = (
+        "id",
+        "display_name",
+        "area",
+    )
+
+admin.site.register(GeoAddress, GeoAddressAdmin)
 # Register your models here.
