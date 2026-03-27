aa7a405992ee5543436644f8d1f73aa5ab737847
metacortex687 <metacortex687@gmail.com>
2026-03-27 20:53:40 +0700

    Элементы теперь рендерятся внутрь родительского элемента. Сделано аналогичнокак как  Map в leafletjs

--- taxi_manager/frontend/static/js/api/render.js
+++ taxi_manager/frontend/static/js/api/render.js
@@ -1,38 +1,53 @@
 import {htmlIdField} from "../entity.js"
 
-const renderInputField = async (field, entity) => {
+const createElementFromHTML = (html) => {
+    const template = document.createElement("template")
+    template.innerHTML = html.trim()
+    return template.content.firstElementChild
+}
+
+const createDefaultWrapper = (parentElement) => {
+    const wrapper = createElementFromHTML(`
+        <div class="mb-3"></div>
+    `)
+    parentElement.appendChild(wrapper)
+    return wrapper
+}
+
+const renderInputField = async (field, entity, parentElement) => {
     const id = htmlIdField(field)
     const value = entity[field["source_name"]] || ""
     const label = field.label
 
-    return `
-        <div class="mb-3">
+    const wrapper = createDefaultWrapper(parentElement)
+    wrapper.innerHTML =`
             <label for="${id}" class="form-label">${label}</label>
             <input type="text" class="form-control" id="${id}" value="${value}">
-        </div> 
     `
 }
 
-const renderInputDateTimeField = async (field, entity) => {
+const renderInputDateTimeField = async (field, entity, parentElement) => {
     const id = htmlIdField(field)
     const value = entity[field["source_name"]] || ""
     const label = field.label
 
-    const date = new Date(value) //Создается в ISO/Z
+    let local_datetime = ""
+    if(value.trim().length !== 0) {
+        const date = new Date(value) //Создается в ISO/Z
+        local_datetime = new Date(
+                date.getTime() - date.getTimezoneOffset() * 60000)
+                .toISOString().slice(0, 16)
+        
+    }
 
-    const local_datetime = new Date(
-            date.getTime() - date.getTimezoneOffset() * 60000)
-            .toISOString().slice(0, 16)
-
-    return `
-        <div class="mb-3">
+    const wrapper = createDefaultWrapper(parentElement)
+    wrapper.innerHTML =`
             <label for="${id}" class="form-label">${label}</label>
             <input type="datetime-local" class="form-control" id="${id}" value="${local_datetime}">
-        </div> 
     `
 }
 
-const renderSelectedField = async (field, entity) => {
+const renderSelectedField = async (field, entity, parentElement) => {
     const idHtml = htmlIdField(field)
     const items = (await fetch_data(field.options.source)).results
     const placeholderText = field.options.placeholder
@@ -49,17 +64,16 @@ const renderSelectedField = async (field, entity) => {
         [renderPlaceholderOption(), ...items.map(renderOption)]
         .join("")
 
-    return `
-        <div class="mb-3">
+    const wrapper = createDefaultWrapper(parentElement)
+    wrapper.innerHTML =`
             <label for="${idHtml}" class="form-label">${field.label}</label>
             <select class="form-select" id="${idHtml}">                
                 ${renderOptions()}
             </select>
-        </div>
     `
 }
 
-const renderMultySelectedDriverField = (form_drivers_data) => {
+const renderMultySelectedDriverField = (form_drivers_data, parentElement) => {
 
     const lis = form_drivers_data.map(record => `
             <li class="list-group-item d-flex justify-content-between align-items-center js-removed-item">
@@ -69,14 +83,12 @@ const renderMultySelectedDriverField = (form_drivers_data) => {
     `).join("")
 
 
-
-    return `
-        <div class="mb-3">
+    const wrapper = createDefaultWrapper(parentElement)
+    wrapper.innerHTML = `
             <label for="inputDrivers" class="form-label">Водители:</label>
             <ul class="list-group" id="inputDrivers">
             ${lis}
-            </ul>
-        </div>             
+            </ul>      
     `
 }
 
