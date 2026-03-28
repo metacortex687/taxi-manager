f7952f1e0a006f99985455e9efd06d7702a354c8
metacortex687 <metacortex687@gmail.com>
2026-03-28 22:16:58 +0700

    Подгрузка точек с id поездки на карту

--- taxi_manager/frontend/static/js/vehicle-trips.js
+++ taxi_manager/frontend/static/js/vehicle-trips.js
@@ -2,7 +2,8 @@ import { renderMapField } from "./api/render-map.js";
 
 const form = {
         // entity: "/api/v1/vehicles/<int:pk>/trip-points/?from=2026-03-01T00:00:00%2B03:00&to=2026-03-31T23:59:59%2B03:00&offset=41",
-        entity: "/api/v1/vehicles/<int:pk>/locations/?response_format=geojson",        
+        // entity: "/api/v1/vehicles/<int:pk>/locations/?response_format=geojson",   
+        entity: "/api/v1/vehicles/<int:pk>/trip-points/?from=2026-03-01T00:00:00%2B03:00&to=2026-03-31T23:59:59%2B03:00&&response_format=geojson",    
         fields: [
             {
                 source_name: "list",
