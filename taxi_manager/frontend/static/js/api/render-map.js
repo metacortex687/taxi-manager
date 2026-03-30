eb8dafbb80776adec35693fb2a6750fa4f40e9b3
metacortex687 <metacortex687@gmail.com>
2026-03-30 15:07:07 +0700

    Маршруты отрисовываются линиями с маркерами по краям.

--- taxi_manager/frontend/static/js/api/render-map.js
+++ taxi_manager/frontend/static/js/api/render-map.js
@@ -1,6 +1,6 @@
 import { createDefaultWrapper } from "./render.js"
 
-const renderMapField = (field, entity, parentElement, eventData = {id: undefined}) => {
+const renderMapField = (field, entity, parentElement, eventData = { id: undefined }) => {
 
     console.log(entity)
 
@@ -10,57 +10,157 @@ const renderMapField = (field, entity, parentElement, eventData = {id: undefined
     // wrapper.style.cssText = field.cssText
     wrapper.style.cssText = "width: 800px; height: 500px;"
 
-
-
-
     var map = L.map(wrapper, { attributionControl: false });
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    // Отрисовка линий маршрутов
+    // {
+        
 
-    // L.marker([51.5, -0.09]).addTo(map)
-    //     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    //     .openPopup();
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-    const grayIcon = L.icon({
-        iconUrl: "/frontend/static/img/marker-gray.png",
-        shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41],
-        className: "marker-gray"
-    })
 
-    const greenIcon = L.icon({
-        iconUrl: "/frontend/static/img/marker-green.png", 
-        shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41],
-        className: "marker-green"
-    })
+        const geoLayer = L.geoJSON(entity.results).addTo(map);
+    // }
 
-    const geoLayer = L.geoJSON(entity.results, {
-        pointToLayer: function (feature, latlng) {
+    // Отрисовка стартовых точек маршрута
+    // {
+        const grayIconEnd = L.icon({
+            iconUrl: "/frontend/static/img/marker-gray-start.png",
+            shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41],
+            className: "marker-gray"
+        })
 
-            if (eventData && feature.properties.trip === eventData.id) {
-                return L.marker(latlng, { icon:  greenIcon})
+        const greenIconEnd  = L.icon({
+            iconUrl: "/frontend/static/img/marker-green-start.png",
+            shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41],
+            className: "marker-green"
+        })
+
+        let start_points = []
+        entity.results.features.forEach(
+            (feature) => {
+                if (feature.geometry.type === "LineString") {
+                    start_points.push(
+                        {
+                            type: "Feature",
+                            geometry: {
+                                type: "Point",
+                                coordinates: feature.geometry.coordinates[0],
+                            },
+                            properties: {
+                                trip: feature.properties.trip
+                            }
+                        }
+                    )
+
+                }
+
             }
+        )
+        const geoJSONstartPoints = {
+            type: 'FeatureCollection',
+            features: start_points
 
-            return L.marker(latlng, { icon:  grayIcon})
-            
         }
-    }).addTo(map);
 
-    const bounds = geoLayer.getBounds()
-    if (bounds.isValid()) {
-         map.fitBounds(bounds)
-         return
-    }
-    map.setView([61, 105], 3)   
+        L.geoJSON(geoJSONstartPoints
+            ,
+            {
+                pointToLayer: function (feature, latlng) {
+
+                    if (eventData && feature.properties.trip === eventData.id) {
+                        return L.marker(latlng, { icon: greenIconEnd  })
+                    }
+
+                    return L.marker(latlng, { icon: grayIconEnd  })
+
+                }
+            }).addTo(map);
+    // }
+
+    // Отрисовка конечных точек маршрута
+    // {
+        const grayIconStart = L.icon({
+            iconUrl: "/frontend/static/img/marker-gray-end.png",
+            shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41],
+            className: "marker-gray"
+        })
+
+        const greenIconStart = L.icon({
+            iconUrl: "/frontend/static/img/marker-green-end.png",
+            shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41],
+            className: "marker-green"
+        })
+
+        let end_points = []
+        entity.results.features.forEach(
+            (feature) => {
+                if (feature.geometry.type === "LineString") {
+                    end_points.push(
+                        {
+                            type: "Feature",
+                            geometry: {
+                                type: "Point",
+                                coordinates: feature.geometry.coordinates[feature.geometry.coordinates.length-1],
+                            },
+                            properties: {
+                                trip: feature.properties.trip
+                            }
+                        }
+                    )
+
+                }
+
+            }
+        )
+        const geoJSONendPoints = {
+            type: 'FeatureCollection',
+            features: end_points
+
+        }
+        console.log(geoJSONendPoints)
+        L.geoJSON(geoJSONendPoints
+            ,
+            {
+                pointToLayer: function (feature, latlng) {
+
+                    if (eventData && feature.properties.trip === eventData.id) {
+                        return L.marker(latlng, { icon: greenIconStart  })
+                    }
+
+                    return L.marker(latlng, { icon: grayIconStart  })
+
+                }
+            }).addTo(map);
+    // } 
+
+    //Позиционирование карты
+    // {
+        const bounds = geoLayer.getBounds()
+        if (bounds.isValid()) {
+            map.fitBounds(bounds)
+            return
+        }
+        map.setView([61, 105], 3)
+    // }
 
 }
 
