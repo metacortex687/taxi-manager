40062e2f118e3e89ae682736203a267b6fe0bf4b
metacortex687 <metacortex687@gmail.com>
2026-03-31 20:21:19 +0700

    test: Возможность тестировать импорт из одной базы данных тестовой, в другую тестову.

--- taxi_manager/exchange/admin.py
+++ taxi_manager/exchange/admin.py
@@ -5,9 +5,22 @@ from taxi_manager.time_zones.models import TimeZone
 from import_export import resources
 
 
-class TimeZoneResource(resources.ModelResource):
+class SelectableDBAAliasResourceMixin: #Нужен для тестов, что бы выбирать базу данных
+    db_alias: str
 
+    def __init__(self, db_alias: str = "default", **kwargs):
+        super().__init__(**kwargs)
+        self.db_alias = db_alias
+
+    def get_queryset(self):
+        return self._meta.model.objects.using(self.db_alias).all()
+
+    def do_instance_save(self, instance, is_create):
+        instance.save(using=self.db_alias)
+
+
+class TimeZoneResource(SelectableDBAAliasResourceMixin, resources.ModelResource):
     class Meta:
         model = TimeZone
 
-# Register your models here.
+
--- taxi_manager/exchange/tests.py
+++ taxi_manager/exchange/tests.py
@@ -8,6 +8,7 @@ from django.db.models import QuerySet
 import tablib
 from import_export import resources
 
+
 from django.test import TestCase
 
 #uv run manage.py test taxi_manager.exchange.tests.ExchangeTest --settings=taxi_manager.exchange.settings_test
@@ -15,28 +16,55 @@ from django.test import TestCase
 class ExchangeTest(TestCase):
     databases = {"default", "import_target"}
 
-    def get_queryset_a(self, model) -> QuerySet:
-        return model._default_manager.using("default")
+    def get_queryset_a(self, model_class) -> QuerySet:
+        return model_class._default_manager.using("default")
     
-    def get_queryset_b(self, model) -> QuerySet:
-        return model._default_manager.using("import_target") 
+    def get_queryset_b(self, model_class) -> QuerySet:
+        return model_class._default_manager.using("import_target") 
+
+    def get_resource_a(self, resource_class) -> resources.ModelResource:
+        return resource_class(db_alias="default")
+    
+    def get_resource_b(self, resource_class) -> resources.ModelResource:
+        return resource_class(db_alias="import_target")
 
 
     def setUp(self):
         self.time_zone = self.get_queryset_a(TimeZone).create(code="UTC", utc_offset=0)
         self.enterprise1 = self.get_queryset_a(Enterprise).create(name="enterprise1", city="city", time_zone=self.time_zone)
         
-        
+
     def test_time_zone_export_json_csv(self):
-        dataset = TimeZoneResource().export(self.get_queryset_a(TimeZone).all())
+        dataset = self.get_resource_a(TimeZoneResource).export()
 
         self.assertTrue(len(dataset) > 0)
         self.assertTrue(len(dataset.json) > 0)
         self.assertTrue(len(dataset.csv) > 1) #1-я это строка заголовок
 
+
     def test_can_use_two_databases(self):
         self.assertEqual(1, self.get_queryset_a(Enterprise).count())
         self.assertEqual(0, self.get_queryset_b(Enterprise).count())
+        
+
+    def test_import_export_time_zone(self):        
+
+        self.assertEqual(1, self.get_queryset_a(TimeZone).count())
+        self.assertEqual(0, self.get_queryset_b(TimeZone).count())
+
+        dataset = self.get_resource_a(TimeZoneResource).export()
+        self.assertTrue(len(dataset) > 0)
+
+        self.get_resource_b(TimeZoneResource).import_data(dataset, dry_run=False,)
+
+        self.assertEqual(1, self.get_queryset_a(TimeZone).count())
+        self.assertEqual(1, self.get_queryset_b(TimeZone).count())
+
+
+
+
+
+
 
 
 
