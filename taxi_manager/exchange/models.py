2eac628ee56890f867b109c3f010b736d7711610
metacortex687 <metacortex687@gmail.com>
2026-03-31 21:21:17 +0700

    Создана таблица в которой будут хранится ссылки в виде уникальных uuid на загруженные и выгруженные объекты.

--- taxi_manager/exchange/models.py
+++ taxi_manager/exchange/models.py
@@ -1,3 +1,29 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
 from django.db import models
 
-# Create your models here.
+import uuid
+
+class ExchangeItem(models.Model):
+    uuid = models.UUIDField(default = uuid.uuid4, unique=True)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveBigIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+
+    def __str__(self):
+        return f"{self.content_type} #{self.object_id} [{self.uuid}]"
+
+    class Meta:
+        
+        indexes = [
+            models.Index(fields=["content_type", "object_id"]),
+        ]
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=["content_type", "object_id"],
+                name="unique_exchange_object"
+            )
+        ]
