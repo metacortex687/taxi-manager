697c4d86f23b4e2db5c1e965b047ad277eb9b929
metacortex687 <metacortex687@gmail.com>
2026-03-31 12:41:02 +0700

    test: Данные о временных зонах можно экспортировать в форматах json и csv

--- taxi_manager/exchange/admin.py
+++ taxi_manager/exchange/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+from taxi_manager.enterprise.models import Enterprise
+from taxi_manager.time_zones.models import TimeZone
+
+
+from import_export import resources
+
+
+class TimeZoneResource(resources.ModelResource):
+
+    class Meta:
+        model = TimeZone
 
 # Register your models here.
--- taxi_manager/exchange/tests.py
+++ taxi_manager/exchange/tests.py
@@ -1,11 +1,29 @@
-from django.test import TestCase
-from taxi_manager.time_zones.models import TimeZone
 from taxi_manager.enterprise.models import Enterprise
+from taxi_manager.time_zones.models import TimeZone
 
-class ImportExportTest(TestCase):
+from .admin import TimeZoneResource
+
+import tablib
+from import_export import resources
+
+from django.test import TestCase
+
+
+class ExchangeTest(TestCase):
     def setUp(self):
         self.time_zone = TimeZone.objects.create(code="UTC", utc_offset=0)
         self.enterprise1 = Enterprise.objects.create(name="enterprise1", city="city")
         
-    def test_OK(self):
-        pass
+    def test_time_zone_export(self):
+        dataset = TimeZoneResource().export()
+
+        self.assertTrue(len(dataset) > 0)
+        self.assertTrue(len(dataset.json) > 0)
+        self.assertTrue(len(dataset.csv) > 1) #1-я это строка заголовок
+
+
+
+
+
+
+        
