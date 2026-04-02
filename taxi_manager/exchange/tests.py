0dc20e8218f64165097510d4efb741d6316d5495
metacortex687 <metacortex687@gmail.com>
2026-04-02 13:20:48 +0700

    test: при повторном импорте автомобиля в БД не должно быть задвоения записей

--- taxi_manager/exchange/tests.py
+++ taxi_manager/exchange/tests.py
@@ -391,44 +391,24 @@ class ExchangeVehicleTest(TestCase):
 
         self.assertEqual(0, Vehicle.objects.all().count())
 
-    # def test_import_enterprise_creates_exchange_item(self):
-    #     self.assertEqual(1, TimeZone.objects.all().count())
-    #     self.assertEqual(1, Enterprise.objects.all().count())
+    def test_vehicle_import_only_once(self):
+        dataset_time_zone = TimeZoneResource().export()
+        dataset_enterprise = EnterpriseResource().export()
+        dataset_model = ModelResource().export()
+        dataset_vehicle = VehicleResource().export()
 
-    #     dataset_time_zone = TimeZoneResource().export()
-    #     dataset_enterprise = EnterpriseResource().export()
+        clear_db()
+        self.assertEqual(0, Vehicle.objects.all().count())
 
-    #     clear_db()
+        TimeZoneResource().import_data(dataset_time_zone, raise_errors=True)
+        EnterpriseResource().import_data(dataset_enterprise, raise_errors=True)
+        ModelResource().import_data(dataset_model, raise_errors=True)
+        VehicleResource().import_data(dataset_vehicle, raise_errors=True)
 
-    #     TimeZoneResource().import_data(dataset_time_zone, raise_errors=True)
-    #     EnterpriseResource().import_data(dataset_enterprise, raise_errors=True)
+        self.assertEqual(1, Vehicle.objects.all().count())
 
-    #     self.assertTrue(
-    #         ExchangeItem.objects.filter(
-    #             uuid=dataset_enterprise["exchange_uuid"][0],
-    #             content_type=ContentType.objects.get_for_model(Enterprise),
-    #         ).exists()
-    #     )
-
-    # def test_import_enterprise_only_once(self):
-    #     self.assertEqual(1, TimeZone.objects.all().count())
-    #     self.assertEqual(1, Enterprise.objects.all().count())
-
-    #     dataset_time_zone = TimeZoneResource().export()
-    #     dataset_enterprise = EnterpriseResource().export()
-
-    #     clear_db()
-    #     self.assertEqual(0, TimeZone.objects.all().count())
-    #     self.assertEqual(0, Enterprise.objects.all().count())
-
-    #     TimeZoneResource().import_data(dataset_time_zone, raise_errors=True)
-    #     EnterpriseResource().import_data(dataset_enterprise, raise_errors=True)
-
-    #     self.assertEqual(1, TimeZone.objects.all().count())
-    #     self.assertEqual(1, Enterprise.objects.all().count())
-
-    #     EnterpriseResource().import_data(dataset_enterprise, raise_errors=True)
-    #     self.assertEqual(1, Enterprise.objects.all().count())
+        VehicleResource().import_data(dataset_vehicle, raise_errors=True)
+        self.assertEqual(1, Vehicle.objects.all().count())
 
     # def test_import_enterprise_updates_existing_object(self):
     #     self.assertEqual(1, TimeZone.objects.all().count())
