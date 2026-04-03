1eb4456db2ba8d4936f6d38fa5a5db06082d9900
metacortex687 <metacortex687@gmail.com>
2026-04-03 20:25:00 +0700

    refactor: Метод фронтенда выполнения запросов с отправкой токена, вынес в отдельный модуль.

---
+++ taxi_manager/frontend/static/js/api/fetch-data.js
@@ -0,0 +1,26 @@
+const fetch_data = async (url, method = "GET", body=undefined) => {
+    const tokenAuth = localStorage.getItem("tokenAuth")
+
+    clear_massages()
+
+    const response = await fetch(url, {
+        method: method,
+        headers: { "Accept": "application/json", "Content-Type": "application/json", "Authorization": `Token ${tokenAuth}` },
+        body: body
+    })
+
+    const data = response.statusText === "No Content" ? {} : await response.json()
+
+    if (!response.ok) {
+        console.log(data)
+        if (data.detail === undefined) {  
+            allert_message(JSON.stringify(data))                 
+            return data
+        }
+
+        allert_message(data.detail)
+        
+    } 
+    
+    return data
+} 
