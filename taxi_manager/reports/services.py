f358ad90c3b6324b0ec4f25efa894ec387be097f
metacortex687 <metacortex687@gmail.com>
2026-04-07 21:27:32 +0700

    refactor: получение списка доступных отчетов перенесено в ReportService

--- taxi_manager/reports/services.py
+++ taxi_manager/reports/services.py
@@ -1,6 +1,17 @@
 import uuid
+from . import models
 
-class ReportService():
+
+class ReportService:
     def create_report(self, report_type, params) -> uuid.UUID:
         return uuid.uuid4()
 
+    def get_available_reports(self):
+        [
+            {
+                "name": report_type["name"],
+                "verbose_name": report_type["verbose_name"],
+                "params": report_type["params"],
+            }
+            for report_type in models.Report.get_report_types()
+        ]
--- taxi_manager/reports/views.py
+++ taxi_manager/reports/views.py
@@ -2,22 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from . import models
+
 from . import services
 
 
 class ReportListAPIView(APIView):
     def get(self, request):
-        return Response(
-            [
-                {
-                    "name": report_type["name"],
-                    "verbose_name": report_type["verbose_name"],
-                    "params": report_type["params"],
-                }
-                for report_type in models.Report.get_report_types()
-            ]
-        )
+        return Response(services.ReportService().get_available_reports())
 
 
 class ReportAPIView(APIView):
@@ -26,6 +17,11 @@ class ReportAPIView(APIView):
         uuid = services.ReportService().create_report(report_type, params)
 
         return Response(
-            {"uuid": str(uuid), "type": report_type, "status": "done", "params": params},
-            status=status.HTTP_201_CREATED
+            {
+                "uuid": str(uuid),
+                "type": report_type,
+                "status": "done",
+                "params": params,
+            },
+            status=status.HTTP_201_CREATED,
         )
