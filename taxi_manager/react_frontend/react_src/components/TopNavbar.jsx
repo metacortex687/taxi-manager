e959820330046a9bf7c4d7f04b3ae6e73be55cce
metacortex687 <metacortex687@gmail.com>
2026-04-20 09:05:17 +0700

    fix: убран старый код, который выдавал ошибку в случае не авторизованного пользователя, я теперь информацию об авторизации пользователя беру из context

--- taxi_manager/react_frontend/react_src/components/TopNavbar.jsx
+++ taxi_manager/react_frontend/react_src/components/TopNavbar.jsx
@@ -1,4 +1,3 @@
-import {loadUserInfo, logout} from "../api/users.js"
 import AuthContext from "../auth/AuthContest"
 
 import React from "react"
@@ -6,17 +5,10 @@ import React from "react"
 class TopNavbar extends React.Component {    
     static contextType = AuthContext
 
-    async componentDidMount() {
-        const userInfo = await loadUserInfo()
-        this.setState({ userInfo })
-    }
-
     handleSubmitLogout = (e) => {
         e.preventDefault()
         this.context.logout()
     }
-
-
 
     render() {
         const {isAuthenticated, userInfo} = this.context
