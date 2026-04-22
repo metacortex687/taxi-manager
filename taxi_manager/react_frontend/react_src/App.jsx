ca424eeb5859e073d455a6ca22cfe46b69c06432
metacortex687 <metacortex687@gmail.com>
2026-04-22 07:09:20 +0700

    refactor: QueryClientProvider перенесен из index в App

--- taxi_manager/react_frontend/react_src/App.jsx
+++ taxi_manager/react_frontend/react_src/App.jsx
@@ -8,23 +8,35 @@ import RequireAuth from "./auth/RequireAuth"
 
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60_000, // 1 минута без лишних перезапросов
+    },
+  },
+})
+
 
 function App() {
   return (
-    <BrowserRouter basename="/react">
-      <AuthProvider>
-        <TopNavbar />
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route path="/login" element={<LoginPage />} />
-            <Route element={<RequireAuth/>}>
-              <Route index element={<HelloWorldPage />} /> 
-              <Route path="enterprises" element={<EnterprisesPage/>} />             
-            </Route>            
-          </Route>
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter basename="/react">
+        <AuthProvider>
+          <TopNavbar />
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route path="/login" element={<LoginPage />} />
+              <Route element={<RequireAuth/>}>
+                <Route index element={<HelloWorldPage />} /> 
+                <Route path="enterprises" element={<EnterprisesPage/>} />             
+              </Route>            
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
--- taxi_manager/react_frontend/react_src/index.jsx
+++ taxi_manager/react_frontend/react_src/index.jsx
@@ -1,20 +1,10 @@
 import { createRoot } from "react-dom/client"
 import App from "./App.jsx"
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 60_000, // 1 минута без лишних перезапросов
-    },
-  },
-})
 
 
 const root = createRoot(document.getElementById("react-root"))
 root.render(
-  <QueryClientProvider client={queryClient}>
     <App />
-  </QueryClientProvider>
 )
