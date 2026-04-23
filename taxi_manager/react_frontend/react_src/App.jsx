6fc3a04d9d87a184d92bebdafe58efd41aaae19c
metacortex687 <metacortex687@gmail.com>
2026-04-23 10:03:17 +0700

    Подключены в приложение подключены Toaster и MantineProvider

--- taxi_manager/react_frontend/react_src/App.jsx
+++ taxi_manager/react_frontend/react_src/App.jsx
@@ -6,8 +6,17 @@ import LoginPage from "./pages/LoginPage"
 import HelloWorldPage from "./pages/HelloWorldPage"
 import EnterprisesPage from "./pages/EnterprisesPage"
 import VehiclesPage from "./pages/VehiclesPage"
+import TripsExportPage from "./pages/TripsExportPage"
 
 import RequireAuth from "./auth/RequireAuth"
+
+import '@mantine/core/styles.css'
+import '@mantine/dates/styles.css'
+import 'dayjs/locale/ru'
+import { MantineProvider } from '@mantine/core'
+import { DatesProvider } from '@mantine/dates'
+
+import { Toaster } from 'react-hot-toast'
 
 
 import routes from "./routes"
@@ -27,23 +36,35 @@ const queryClient = new QueryClient({
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename="/react">
-        <AuthProvider>
-          <TopNavbar />
-          <Routes>
-            <Route element={<AppLayout />}>
-              <Route path={routes.login.path} element={<LoginPage />} />
-              <Route element={<RequireAuth/>}>
-                <Route index element={<HelloWorldPage />} /> 
-                <Route path={routes.enterprises.path} element={<EnterprisesPage/>} /> 
-                <Route path={routes.vehicles.path} element={<VehiclesPage/>} />             
-              </Route>            
-            </Route>
-          </Routes>
-        </AuthProvider>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <MantineProvider>
+      <DatesProvider settings={{ locale: 'ru' }}>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter basename="/react">
+            <AuthProvider>
+              <TopNavbar />
+              <Toaster
+                position="top-center"
+                toastOptions={{
+                  duration: 4000,
+                }}
+              />
+              <Routes>
+                <Route element={<AppLayout />}>
+                  <Route path={routes.login.path} element={<LoginPage />} />
+                  <Route element={<RequireAuth/>}>
+                    <Route index element={<HelloWorldPage />} /> 
+                    <Route path={routes.enterprises.path} element={<EnterprisesPage/>} /> 
+                    <Route path={routes.vehicles.path} element={<VehiclesPage/>} /> 
+                    <Route path={routes.trips.export.path} element={<TripsExportPage/>} />             
+                  </Route>            
+                </Route>
+              </Routes>
+            </AuthProvider>
+          </BrowserRouter>
+        </QueryClientProvider>
+      </DatesProvider>
+    </MantineProvider>
+
   )
 }
 
