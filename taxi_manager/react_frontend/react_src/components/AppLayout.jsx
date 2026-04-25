f6ad7506242ae584d0fad6c0a73d9c104162a0f3
metacortex687 <metacortex687@gmail.com>
2026-04-25 10:46:44 +0700

    fix: элементы страницы расползались на всю ширину экрана

--- taxi_manager/react_frontend/react_src/components/AppLayout.jsx
+++ taxi_manager/react_frontend/react_src/components/AppLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom"
 class AppLayout extends React.Component {
   render() {
     return (
-      <div className="d-flex flex-column min-vh-100">
+      <div className="container py-3">
         <Outlet />
       </div>
     )
