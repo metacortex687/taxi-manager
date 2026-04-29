44d726a2580b16a8090dbbade54a65f7b31adb6b
metacortex687 <metacortex687@gmail.com>
2026-04-29 09:54:06 +0700

    chore: команды вынесены в Makefile

---
+++ Makefile
@@ -0,0 +1,4 @@
+start-vite:
+	echo "Starting Vite watcher..."
+	cd /home/taxi-manager/taxi_manager/react_frontend && npm run dev -- --host 0.0.0.0
+	
--- start_vite.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Starting Vite watcher..."
-cd /home/taxi-manager/taxi_manager/react_frontend || exit 1
-npm run dev -- --host 0.0.0.0
