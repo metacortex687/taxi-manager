6a717c76c2b099c1943fb5fb79eb00a85cd70fad
metacortex687 <metacortex687@gmail.com>
2026-05-07 20:08:54 +0700

    Вернул использование переменных окруженияю Тесты оплучитс сделать только с Postgis, для чего нужны настройки.

--- taxi_manager/settings.py
+++ taxi_manager/settings.py
@@ -101,26 +101,7 @@ WSGI_APPLICATION = 'taxi_manager.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES["default"].update(
-    db_from_env
-)  # update from DATABASE_URL in .env if defined
-DATABASES["default"]["ENGINE"] = "django.contrib.gis.db.backends.postgis"
-DATABASES["default"]["TEST"]  = {
-    'NAME': 'mytestdatabase',    
+    "default": dj_database_url.config(conn_max_age=600, engine="django.contrib.gis.db.backends.postgis", test_options={'NAME': 'mytestdatabase'})
 }
 
 # Password validation
