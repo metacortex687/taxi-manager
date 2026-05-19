d151527d20bf392777fe4196182e62ef84af1499
metacortex687 <metacortex687@gmail.com>
2026-05-19 21:37:16 +0700

    Добавлена модель для привязки пользователя VK к пользователю Django

--- taxi_manager/infrastructure/vk_bot/models.py
+++ taxi_manager/infrastructure/vk_bot/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+class VkUser(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    vk_user_id = models.CharField(max_length=15, unique=True)
+
