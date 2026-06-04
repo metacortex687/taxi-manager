ed84136695cd142625762900d17453cad592eda9
metacortex687 <metacortex687@gmail.com>
2026-06-04 11:55:34 +0700

    Сообщение о замерах получения из кэша и из базы данных  теперь содержат trace_id

--- taxi_manager/infrastructure/cache_manager/decorators.py
+++ taxi_manager/infrastructure/cache_manager/decorators.py
@@ -1,9 +1,12 @@
+import logging
 import time
 from functools import wraps
 
 from taxi_manager.infrastructure.cache_manager.services import CacheManager
 
 cache_manager = CacheManager()
+
+logger = logging.getLogger(__name__)
 
 
 def cached_method(key_fn, timeout=None):
@@ -17,19 +20,16 @@ def cached_method(key_fn, timeout=None):
             elapsed = time.perf_counter() - start
 
             if not cache_manager.is_missing(value):
-                print(f"Cache get HIT key={key}: {elapsed * 1000:.3f} ms")
+                logger.info("Cache get HIT key=%s: %.3f ms", key, elapsed * 1000)
                 return value
 
-            print(f"Cache get MISS key={key}: {elapsed * 1000:.3f} ms")
+            logger.info("Cache get MISS key=%s: %.3f ms", key, elapsed * 1000)
 
-            if not cache_manager.is_missing(value):
-                return value
-            
             start = time.perf_counter()
             value = method(self, *args, **kwargs)
             value_load_elapsed = time.perf_counter() - start
 
-            print(f"Value load key={key}: {value_load_elapsed * 1000:.3f} ms")
+            logger.info("Value load key=%s: %.3f ms", key, value_load_elapsed * 1000)
 
             cache_manager.set(key, value, timeout=timeout)
 
