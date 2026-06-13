02c23de5bcd0b9d5e8c4a99583bda2da26524ac4
metacortex687 <metacortex687@gmail.com>
2026-06-13 11:53:41 +0700

    В DjangoUnitOfWork добавлена поддержка транзакции с уровнем изоляции REPEATABLE READ

--- taxi_manager/application/unit_of_work.py
+++ taxi_manager/application/unit_of_work.py
@@ -5,3 +5,6 @@ from typing import Protocol
 class IUnitOfWork(Protocol):
     def transaction(self) -> AbstractContextManager:
         pass
+
+    def read_only_transaction(self) -> AbstractContextManager:
+        pass
--- taxi_manager/infrastructure/repositories/unit_of_work.py
+++ taxi_manager/infrastructure/repositories/unit_of_work.py
@@ -6,3 +6,14 @@ from taxi_manager.application.unit_of_work import IUnitOfWork
 class DjangoUnitOfWork(IUnitOfWork):
     def transaction(self):
         return transaction.atomic()
+
+    @contextmanager
+    def read_only_transaction(self):
+         # согласованное чтение, не блокирует базу данных
+        with transaction.atomic():
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "SET TRANSACTION ISOLATION LEVEL REPEATABLE READ READ ONLY"
+                )
+
+            yield
