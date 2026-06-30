d13d356779a7009b0f4ffde02274a7ed7b864f01
metacortex687 <metacortex687@gmail.com>
2026-06-30 21:04:50 +0700

    Добавлена трасировка методов получении маршрутов, что бы оценить вклад сериализации в задержку

--- taxi_manager/infrastructure/api_v1/serializers/trip.py
+++ taxi_manager/infrastructure/api_v1/serializers/trip.py
@@ -1,10 +1,14 @@
+import json
+
 from rest_framework import serializers
 from zoneinfo import ZoneInfo
 from rest_framework_gis.serializers import (
     GeoFeatureModelSerializer,
     GeometrySerializerMethodField,
 )
+from taxi_manager.infrastructure.api_v1.serializers.trace import TracedGeometrySerializerMethodField
 from taxi_manager.infrastructure.geo_tracking.models import VehicleLocation, Trip
+from taxi_manager.infrastructure.observability.tracing import trace_method
 
 
 class TripPointSerializer(serializers.Serializer):
@@ -13,6 +17,7 @@ class TripPointSerializer(serializers.Serializer):
     tracked_at = serializers.DateTimeField(read_only=True)
     location = serializers.CharField(read_only=True)
 
+    @trace_method("TripPointSerializer.to_representation")
     def to_representation(self, instance):
         self.fields["tracked_at"] = serializers.DateTimeField(
             default_timezone=ZoneInfo(instance.vehicle.enterprise.time_zone.code)
@@ -22,16 +27,22 @@ class TripPointSerializer(serializers.Serializer):
 
 class TripPointSerializerGeoJSON(GeoFeatureModelSerializer):
     trip = serializers.IntegerField(source="id", read_only=True)
-    route = GeometrySerializerMethodField()
+    route = TracedGeometrySerializerMethodField(
+        span_name="TripPointSerializerGeoJSON.route.to_representation",
+        stage="serialize",
+    )
 
+    @trace_method("TripPointSerializerGeoJSON.get_route", stage="serialize")
     def get_route(self, obj):
         return obj.path
 
-    # def to_representation(self, instance):
-    #     self.fields["tracked_at"] = serializers.DateTimeField(
-    #         default_timezone=ZoneInfo(instance.vehicle.enterprise.time_zone.code)
-    #     )
-    #     return super().to_representation(instance)
+    @trace_method("TripPointSerializerGeoJSON.to_representation", stage="serialize")
+    def to_representation(self, instance):
+        return super().to_representation(instance)
+
+    @trace_method("TripPointSerializerGeoJSON.get_properties", stage="serialize")
+    def get_properties(self, instance, fields):
+        return super().get_properties(instance, fields)
 
     class Meta:
         model = Trip
@@ -46,6 +57,7 @@ class TripSerializer(serializers.Serializer):
     ended_at = serializers.DateTimeField(read_only=True)
     end_point = serializers.SerializerMethodField(read_only=True)
 
+    @trace_method("TripSerializer.to_representation")
     def to_representation(self, instance):
         self.fields["started_at"] = serializers.DateTimeField(
             default_timezone=ZoneInfo(instance.vehicle.enterprise.time_zone.code)
@@ -55,6 +67,7 @@ class TripSerializer(serializers.Serializer):
         )
         return super().to_representation(instance)
 
+    @trace_method("TripSerializer.get_start_point")
     def get_start_point(self, obj):
         if obj.start_point is None:
             return None
