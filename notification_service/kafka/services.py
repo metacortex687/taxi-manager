0c3d39f673fa9d47ed5b85e2125f175552c8b29a
metacortex687 <metacortex687@gmail.com>
2026-07-18 20:46:25 +0700

    Слушатель kafka обрабатывает все сообщения

--- notification_service/kafka/services.py
+++ notification_service/kafka/services.py
@@ -1,15 +1,22 @@
+import json
+
 from confluent_kafka import Consumer
 
+from notification_context.handlers import handle_assignment_managers, handle_enterprise, handle_vehicle_models
+from notifications.services import notify_enterprise_managers
+from notifications.handlers import handle_vehicles
 
 KAFKA_GROUP_ID = "vk-bot-notification"
 
 KAFKA_TOPICS = [
     "taxi_manager.enterprises",
     "taxi_manager.assignment_managers",
+    "taxi_manager.vehicle_models",
+    "taxi_manager.vehicles"
 ]
 
 
-def start_listener_kafka(kafka_bootstrap_servers: str) -> None:
+def start_listener_kafka(kafka_bootstrap_servers: str, vk_bot_client) -> None:
     consumer = Consumer(
         {
             "bootstrap.servers": kafka_bootstrap_servers,
@@ -19,6 +26,8 @@ def start_listener_kafka(kafka_bootstrap_servers: str) -> None:
     )
 
     consumer.subscribe(KAFKA_TOPICS)
+
+    print("listener_kafka работает")
 
     try:
         while True:
@@ -31,10 +40,54 @@ def start_listener_kafka(kafka_bootstrap_servers: str) -> None:
                 print(f"Ошибка Kafka: {message.error()}")
                 continue
 
-            value = message.value()
+            value_b = message.value()
 
-            if value is not None:
-                print(value.decode("utf-8"))
+            if value_b is None:
+                continue
+
+            topic = message.topic()
+            value_dict = json.loads(value_b.decode("utf-8"))
+
+            if topic == "taxi_manager.enterprises":
+                handle_enterprise(value_dict)
+                continue 
+
+            if topic == "taxi_manager.assignment_managers":
+                handle_assignment_managers(value_dict)
+                continue     
+
+            if topic == "taxi_manager.vehicle_models":
+                handle_vehicle_models(value_dict)
+                continue  
+
+            if topic == "taxi_manager.vehicles":
+                before = value_dict.get("before")
+                after = value_dict.get("after")
+
+                enterprise_uuids = {
+                    vehicle["enterprise_uuid"]
+                    for vehicle in (before, after)
+                    if vehicle and vehicle.get("enterprise_uuid")
+                }
+
+                notify_text = handle_vehicles(value_dict)
+
+                if not notify_text:
+                    continue
+
+                for enterprise_uuid in enterprise_uuids:
+                    notify_enterprise_managers(
+                        enterprise_uuid,
+                        notify_text,
+                        vk_bot_client,
+                    )
+
+                continue 
+
+            
+            print(f"Необработанный топик: {topic}")
+            print(value_dict)
+
 
     except KeyboardInterrupt:
         pass
