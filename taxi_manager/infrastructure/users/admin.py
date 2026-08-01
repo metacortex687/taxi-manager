fb048ef72714d1762adb7c78192d40a4dcc80c1c
metacortex687 <metacortex687@gmail.com>
2026-08-01 16:01:22 +0700

    Админка для новой модели пользователей

--- taxi_manager/infrastructure/users/admin.py
+++ taxi_manager/infrastructure/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-# Register your models here.
+from taxi_manager.infrastructure.users.models import User
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    readonly_fields = (*DjangoUserAdmin.readonly_fields, "uuid")
+
+    fieldsets = DjangoUserAdmin.fieldsets + (
+        ("Дополнительно", {"fields": ("uuid",)}),
+    )
